5eb9b7a585c95f857dafb4646bdca7233b07158d
cptmeeps <cptmeeps@gmail.com>
2024-01-23 23:55:39 -0800

    simplify

--- hf_model.py
+++ hf_model.py
@@ -1,4 +1,4 @@
-""" PyTorch Llava model."""
+# https://github.com/huggingface/transformers/blob/main/src/transformers/models/llava/modeling_llava.py
 from dataclasses import dataclass
 from typing import List, Optional, Tuple, Union
 
@@ -6,7 +6,7 @@ import torch
 import torch.utils.checkpoint
 from torch import nn
 
-
+# model config dataclass
 class LlavaConfig(PretrainedConfig):
 
   model_type = "llava"
@@ -54,7 +54,16 @@ class LlavaConfig(PretrainedConfig):
 
     super().__init__(**kwargs)
 
+# output dataclass
+class LlavaCausalLMOutputWithPast(ModelOutput):
+  loss: Optional[torch.FloatTensor] = None
+  logits: torch.FloatTensor = None
+  past_key_values: Optional[List[torch.FloatTensor]] = None
+  hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+  attentions: Optional[Tuple[torch.FloatTensor]] = None
+  image_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
 
+# input preprocessing
 class LlavaProcessor(ProcessorMixin):
 
   attributes = ["image_processor", "tokenizer"]
@@ -96,15 +105,7 @@ class LlavaProcessor(ProcessorMixin):
     return list(dict.fromkeys(tokenizer_input_names + image_processor_input_names))
 
 
-class LlavaCausalLMOutputWithPast(ModelOutput):
-  loss: Optional[torch.FloatTensor] = None
-  logits: torch.FloatTensor = None
-  past_key_values: Optional[List[torch.FloatTensor]] = None
-  hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-  attentions: Optional[Tuple[torch.FloatTensor]] = None
-  image_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-
-
+# project image features to text features
 class LlavaMultiModalProjector(nn.Module):
   def __init__(self, config: LlavaConfig):
     super().__init__()
@@ -120,7 +121,8 @@ class LlavaMultiModalProjector(nn.Module):
     return hidden_states
 
 
-class LlavaPreTrainedModel(PreTrainedModel): # bare outputs raw h_state
+# base model outputs raw h_state
+class LlavaPreTrainedModel(PreTrainedModel): 
   config_class = LlavaConfig
   base_model_prefix = "model"
   supports_gradient_checkpointing = True
@@ -160,52 +162,22 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     )
     self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
     self.post_init()
-
-  def get_input_embeddings(self):
-    return self.language_model.get_input_embeddings()
-
-  def set_input_embeddings(self, value):
-    self.language_model.set_input_embeddings(value)
-
-  def get_output_embeddings(self):
-    return self.language_model.get_output_embeddings()
-
-  def set_output_embeddings(self, new_embeddings):
-    self.language_model.set_output_embeddings(new_embeddings)
-
-  def set_decoder(self, decoder):
-    self.language_model.set_decoder(decoder)
-
-  def get_decoder(self):
-    return self.language_model.get_decoder()
-
-  def tie_weights(self):
-    return self.language_model.tie_weights()
-
-  def resize_token_embeddings(self, new_num_tokens: Optional[int] = None, pad_to_multiple_of=None) -> nn.Embedding:
-    model_embeds = self.language_model.resize_token_embeddings(new_num_tokens, pad_to_multiple_of)
-    # update vocab size
-    self.config.text_config.vocab_size = model_embeds.num_embeddings
-    self.config.vocab_size = model_embeds.num_embeddings
-    self.vocab_size = model_embeds.num_embeddings
-    return model_embeds
-
+  
   def _merge_input_ids_with_image_features(self, image_features, inputs_embeds, input_ids, attention_mask, labels):
+    
     num_images, num_image_patches, embed_dim = image_features.shape
     batch_size, sequence_length = input_ids.shape
     left_padding = not torch.sum(input_ids[:, -1] == torch.tensor(self.pad_token_id))
+    
     # 1. Create a mask to know where special image tokens are
     special_image_token_mask = input_ids == self.config.image_token_index
     num_special_image_tokens = torch.sum(special_image_token_mask, dim=-1)
-    # Compute the maximum embed dimension
     max_embed_dim = (num_special_image_tokens.max() * (num_image_patches - 1)) + sequence_length
     batch_indices, non_image_indices = torch.where(input_ids != self.config.image_token_index)
 
-    # 2. Compute the positions where text should be written
-    # Calculate new positions for text tokens in merged image-text sequence.
-    # `special_image_token_mask` identifies image tokens. Each image token will be replaced by `nb_text_tokens_per_images - 1` text tokens.
-    # `torch.cumsum` computes how each image token shifts subsequent text token positions.
-    # - 1 to adjust for zero-based indexing, as `cumsum` inherently increases indices by one.
+    # 2. Calculate new positions for text tokens in merged image-text sequence.
+    # special_image_token_mask - identifies image tokens. Each image token will be replaced by `nb_text_tokens_per_images - 1` text tokens.
+    # torch.cumsum - computes how each image token shifts subsequent text token positions. - 1 to adjust for zero-based indexing, as `cumsum` inherently increases indices by one.
     new_token_positions = torch.cumsum((special_image_token_mask * (num_image_patches - 1) + 1), -1) - 1
     nb_image_pad = max_embed_dim - 1 - new_token_positions[:, -1]
     if left_padding:
@@ -213,18 +185,13 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     text_to_overwrite = new_token_positions[batch_indices, non_image_indices]
 
     # 3. Create the full embedding, already padded to the maximum position
-    final_embedding = torch.zeros(
-      batch_size, max_embed_dim, embed_dim, dtype=inputs_embeds.dtype, device=inputs_embeds.device
-    )
-    final_attention_mask = torch.zeros(
-      batch_size, max_embed_dim, dtype=attention_mask.dtype, device=inputs_embeds.device
-    )
+    final_embedding = torch.zeros(batch_size, max_embed_dim, embed_dim, dtype=inputs_embeds.dtype, device=inputs_embeds.device)
+    final_attention_mask = torch.zeros(batch_size, max_embed_dim, dtype=attention_mask.dtype, device=inputs_embeds.device)
     if labels is not None:
       final_labels = torch.full(
         (batch_size, max_embed_dim), self.config.ignore_index, dtype=input_ids.dtype, device=input_ids.device
       )
-    # In case the Vision model or the Language model has been offloaded to CPU, we need to manually
-    # set the corresponding tensors into their correct target device.
+    # If Vision/Language model offload to CPU, then set tensors to correct device
     target_device = inputs_embeds.device
     batch_indices, non_image_indices, text_to_overwrite = (
       batch_indices.to(target_device),
@@ -233,8 +200,7 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     )
     attention_mask = attention_mask.to(target_device)
 
-    # 4. Fill the embeddings based on the mask. If we have ["hey" "<image>", "how", "are"]
-    # we need to index copy on [0, 577, 578, 579] for the text and [1:576] for the image features
+    # 4. Fill embeddings based on mask. ex: ["hey" "<image>", "how", "are"] need to index copy on [0, 577, 578, 579] for the text and [1:576] for the image features
     final_embedding[batch_indices, text_to_overwrite] = inputs_embeds[batch_indices, non_image_indices]
     final_attention_mask[batch_indices, text_to_overwrite] = attention_mask[batch_indices, non_image_indices]
     if labels is not None:
@@ -268,7 +234,6 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     past_key_values: Optional[List[torch.FloatTensor]] = None,
     inputs_embeds: Optional[torch.FloatTensor] = None,
     vision_feature_layer: Optional[int] = None,
-    vision_feature_select_strategy: Optional[str] = None,
     labels: Optional[torch.LongTensor] = None,
     use_cache: Optional[bool] = None,
     output_attentions: Optional[bool] = None,
@@ -280,73 +245,19 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     output_hidden_states = (
       output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
     )
-    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-    vision_feature_layer = (
-      vision_feature_layer if vision_feature_layer is not None else self.config.vision_feature_layer
+
+    inputs_embeds = self.get_input_embeddings()(input_ids)
+    image_outputs = self.vision_tower(pixel_values, output_hidden_states=True)
+    
+    selected_image_feature = image_outputs.hidden_states[vision_feature_layer]
+    selected_image_feature = selected_image_feature[:, 1:]
+    image_features = self.multi_modal_projector(selected_image_feature)
+
+    inputs_embeds, attention_mask, labels, position_ids = self._merge_input_ids_with_image_features(
+      image_features, inputs_embeds, input_ids, attention_mask, labels
     )
-    vision_feature_select_strategy = (
-      vision_feature_select_strategy
-      if vision_feature_select_strategy is not None
-      else self.config.vision_feature_select_strategy
-    )
-
-    if inputs_embeds is None:
-      # 1. Extra the input embeddings
-      inputs_embeds = self.get_input_embeddings()(input_ids)
-
-      # 2. Merge text and images
-      if pixel_values is not None and input_ids.shape[1] != 1:
-        image_outputs = self.vision_tower(pixel_values, output_hidden_states=True)
-        # this is not memory efficient at all (output_hidden_states=True) will save all the hidden stated.
-        selected_image_feature = image_outputs.hidden_states[vision_feature_layer]
-
-        if vision_feature_select_strategy == "default":
-          selected_image_feature = selected_image_feature[:, 1:]
-        elif vision_feature_select_strategy == "full":
-          selected_image_feature = selected_image_feature
-        else:
-          raise ValueError(
-            f"Unexpected select feature strategy: {self.config.vision_feature_select_strategy}"
-          )
-
-        image_features = self.multi_modal_projector(selected_image_feature)
-        inputs_embeds, attention_mask, labels, position_ids = self._merge_input_ids_with_image_features(
-          image_features, inputs_embeds, input_ids, attention_mask, labels
-        )
-        if labels is None:
-          labels = torch.full_like(attention_mask, self.config.ignore_index).to(torch.long)
-      else:
-        # In case input_ids.shape[1] == 1 & pixel_values==None & past_key_values != None, we are in the case of
-        # generation with cache
-        if past_key_values is not None and pixel_values is not None and input_ids.shape[1] == 1:
-          # Retrieve the first layer to inspect the logits and mask out the hidden states
-          # that are set to 0
-          first_layer_past_key_value = past_key_values[0][0][:, :, :, 0]
-
-          # Sum all dimensions of head_dim (-2) to avoid random errors such as: https://github.com/huggingface/transformers/pull/28032#issuecomment-1863691941
-          batch_index, non_attended_tokens = torch.where(first_layer_past_key_value.float().sum(-2) == 0)
-
-          # Get the target length
-          target_seqlen = first_layer_past_key_value.shape[-1] + 1
-
-          extended_attention_mask = torch.ones(
-            (attention_mask.shape[0], target_seqlen - attention_mask.shape[1]),
-            dtype=attention_mask.dtype,
-            device=attention_mask.device,
-          )
-
-          # Filter out only the tokens that can be un-attended, this can happen
-          # if one uses Llava + Fused modules where the cache on the
-          # first iteration is already big enough, or if one passes custom cache
-          valid_indices = non_attended_tokens < extended_attention_mask.size(-1)
-          new_batch_index = batch_index[valid_indices]
-          new_non_attended_tokens = non_attended_tokens[valid_indices]
-
-          # Zero-out the places where we don't need to attend
-          extended_attention_mask[new_batch_index, new_non_attended_tokens] = 0
-
-          attention_mask = torch.cat((attention_mask, extended_attention_mask), dim=1)
-          position_ids = torch.sum(attention_mask, dim=1).unsqueeze(-1) - 1
+    
+    if labels is None:labels = torch.full_like(attention_mask, self.config.ignore_index).to(torch.long)
 
     outputs = self.language_model(
       attention_mask=attention_mask,
@@ -389,58 +300,3 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
       attentions=outputs.attentions,
     )
 
-  def prepare_inputs_for_generation(
-    self, input_ids, past_key_values=None, inputs_embeds=None, pixel_values=None, attention_mask=None, **kwargs
-  ):
-    if past_key_values is not None:
-      if isinstance(past_key_values, Cache):
-        cache_length = past_key_values.get_seq_length()
-        past_length = past_key_values.seen_tokens
-      else:
-        cache_length = past_length = past_key_values[0][0].shape[2]
-
-      # Keep only the unprocessed tokens:
-      # 1 - If the length of the attention_mask exceeds the length of input_ids, then we are in a setting where
-      # some of the inputs are exclusively passed as part of the cache (e.g. when passing input_embeds as
-      # input)
-      if attention_mask is not None and attention_mask.shape[1] > input_ids.shape[1]:
-        input_ids = input_ids[:, -(attention_mask.shape[1] - past_length) :]
-      # 2 - If the past_length is smaller than input_ids', then input_ids holds all input tokens. We can discard
-      # input_ids based on the past_length.
-      elif past_length < input_ids.shape[1]:
-        input_ids = input_ids[:, past_length:]
-      # 3 - Otherwise (past_length >= input_ids.shape[1]), let's assume input_ids only has unprocessed tokens.
-      elif self.config.image_token_index in input_ids:
-        input_ids = input_ids[:, input_ids.shape[1] - 1 :]
-      # If the cache has seen more tokens than it can hold, then the cache has a size limit. Let's discard the
-      # older attention values, as their corresponding values are not part of the input.
-      if cache_length < past_length and attention_mask is not None:
-        attention_mask = attention_mask[:, -(cache_length + input_ids.shape[1]) :]
-
-    position_ids = kwargs.get("position_ids", None)
-    if attention_mask is not None and position_ids is None:
-      # create position_ids on the fly for batch generation
-      position_ids = attention_mask.long().cumsum(-1) - 1
-      position_ids.masked_fill_(attention_mask == 0, 1)
-      if past_key_values:
-        position_ids = position_ids[:, -input_ids.shape[1] :]
-
-    # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
-    if inputs_embeds is not None and past_key_values is None:
-      model_inputs = {"inputs_embeds": inputs_embeds}
-    else:
-      model_inputs = {"input_ids": input_ids}
-
-    model_inputs.update(
-      {
-        "position_ids": position_ids,
-        "past_key_values": past_key_values,
-        "use_cache": kwargs.get("use_cache"),
-        "attention_mask": attention_mask,
-        "pixel_values": pixel_values,
-      }
-    )
-    return model_inputs
-
-  def _reorder_cache(self, *args, **kwargs):
-    return self.language_model._reorder_cache(*args, **kwargs)
